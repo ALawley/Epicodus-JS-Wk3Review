7623e4dbc4d92df557d0051977270ddc8b7e669d
Anna Lawley <alawley1@gmail.com>
2016-04-04 18:05:42 -0700

    add up and down voting of questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,14 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    }
+    },
+    upvote(question) {
+      question.incrementProperty('votes');
+      question.save();
+    },
+    downvote(question) {
+      question.decrementProperty('votes');
+      question.save();
+    },
   }
 });
